1dcea1696ddc3760e9e228a0e39dff0548352bd9
John Mair <jrmair@gmail.com>
2013-06-03 20:12:59 +0200

    Removed web_server_setup.rb code as we now rely on a separate gem to setup the websocket server

--- config/initializers/websocket_server_setup.rb
+++ config/initializers/websocket_server_setup.rb
@@ -1,31 +1,31 @@
-require_relative "pry_protocol"
+# require_relative "pry_protocol"
 
-pry_prot = PryProtocol.new
+# pry_prot = PryProtocol.new
 
-EM.next_tick do
-  @channel = EM::Channel.new
-  @subscriptions = {}
+# EM.next_tick do
+#   @channel = EM::Channel.new
+#   @subscriptions = {}
 
-  EM::WebSocket.start(:host => '0.0.0.0', :port => 3001) do |ws|
-    pry_prot.ws = ws
+#   EM::WebSocket.start(:host => '0.0.0.0', :port => 3001) do |ws|
+#     pry_prot.ws = ws
 
-    ws.onopen do
-      ws.send JSON.dump({ "value" => "web sockets are working!",
-                          "type" => "result"
-                        })
+#     ws.onopen do
+#       ws.send JSON.dump({ "value" => "web sockets are working!",
+#                           "type" => "result"
+#                         })
 
-      subscriber_id = @channel.subscribe do |msg|
-        ws.send msg
-      end
-    end
+#       subscriber_id = @channel.subscribe do |msg|
+#         ws.send msg
+#       end
+#     end
 
-    ws.onmessage do |msg|
-      pry_prot.process_json(msg)
-    end
+#     ws.onmessage do |msg|
+#       pry_prot.process_json(msg)
+#     end
 
-    ws.onclose do
-      @channel.unsubscribe @subscriptions[ws.signature]
-      @subscriptions.delete ws.signature
-    end
-  end
-end
+#     ws.onclose do
+#       @channel.unsubscribe @subscriptions[ws.signature]
+#       @subscriptions.delete ws.signature
+#     end
+#   end
+# end
